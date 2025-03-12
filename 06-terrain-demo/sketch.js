81aec3acfe94ec3545bd451fa1519dab8c3b3225
fe-walker <fewalker.ca@gmail.com>
2025-03-12 13:44:39 -0600

    terrain demo

--- 06-terrain-demo/sketch.js
+++ 06-terrain-demo/sketch.js
@@ -1,15 +1,44 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Generating random terrain using perlin noise
+
+let terrain = [];
+const NUMBER_OF_RECTS = 2000;
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+  generateTerrain(width/NUMBER_OF_RECTS);
 }
 
 function draw() {
-  background(220);
+  background("#b0f1ff");
+  // stroke("#456654");
+  noStroke();
+  fill("#6fa388");
+
+  for (let someRect of terrain){
+    rect(someRect.x, someRect.y, someRect.w, someRect.h);
+  }
+}
+
+function generateTerrain(widthOfRect){
+  let time = 0; 
+  let deltaTime = 0.0001;
+  for (let i = 0;i < NUMBER_OF_RECTS; i++){
+    let theHeight = noise(time) * height;
+    terrain.push(spawnRectangle(i * widthOfRect, theHeight, widthOfRect));
+    time += deltaTime;
+  }
+}
+
+function spawnRectangle(leftSide, rectHeight, rectWidth){
+  let theRect = {
+
+    x: leftSide,
+    y: height - rectHeight,
+    w: rectWidth,
+    h: rectHeight,
+  };
+
+  return theRect;
 }
