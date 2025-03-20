b2b24afd97541b5854765e72ba6f5a96670c8cb5
fe-walker <fewalker.ca@gmail.com>
2025-03-20 13:45:42 -0600

    added commentary

--- array-project/sketch.js
+++ array-project/sketch.js
@@ -5,10 +5,11 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+// to do
+// extra for experts
 
-// to-d0 
-// make it so the shot text goes away after a bit, 
 
+// set variables 
 let x;
 let y;
 let ghostArray = [];
@@ -19,6 +20,7 @@ let deathSpots = [];
 let ghostsShot;
 let waitTime = 5;
 let lastSwitchedTime = 0;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -31,6 +33,7 @@ function draw() {
   showShot();
 }
 
+// array that spawns my ghosts
 function spawnGhost(){
   let someGhost = {
     x: random(50, windowWidth - 50),
@@ -44,6 +47,7 @@ function spawnGhost(){
   ghostArray.push(someGhost);
 }
 
+// all my state screen and images
 function preload(){
   thing = loadImage("ghost-image-transparent.png");
   startScreen = loadImage("start-screen-ghosthunter.png");
@@ -53,17 +57,19 @@ function preload(){
   // add win screen later at home !!
 }
 
+// displays the image of the ghosts
 function displayGhosts(ghost){
   image(ghost.img, ghost.x, ghost.y, ghost.w, ghost.h );
 }
 
+// makes it so that the ghosts get larger as they approach the screen
 function moveGhosts(ghost){
   ghost.w += ghost.speed;
   ghost.h += ghost.speed;
   ghostSize += 1;
 }
 
-
+// changes the state
 function changeScreenIfNeeded(){
   if (screen === "start"){
     displayInstruction();
@@ -102,28 +108,33 @@ function changeScreenIfNeeded(){
   }
 }
 
+// shows instructions to player
 function displayInstruction(){
   image(startScreen, 0, 0, windowWidth, windowHeight);
   fill("white");
   text("Press enter to begin, use mouse to aim and shoot to kill the ghosts", windowWidth/2 -200, windowHeight/2 + 100);
 }
 
+// displays main play screen
 function displayMainScreen(){
   image(mainScreen, windowWidth/2, windowHeight/2, windowWidth, windowHeight);
   fill("white");
   text(ghostsShot, 50, 50);
 }
 
+// death screen
 function displayEndScreen(){
   image(endScreen, windowWidth/2, windowHeight/2, windowWidth, windowHeight);
 }
 
+// win screen
 function displayWinScreen(){
   image(winScreen, windowWidth/2, windowHeight/2, windowWidth, windowHeight);
   fill("white");
   text(ghostsShot, 300, 50);
 }
 
+// function to kill ghosts and make text where i click
 function mousePressed(){
   for (let ghost of ghostArray){
     if (ghost.x - ghost.w/2 < mouseX &&  mouseX < ghost.x + ghost.w/2 && ghost.y - ghost.h/2 < mouseY && mouseY < ghost.y + ghost.h/2){
@@ -140,6 +151,7 @@ function mousePressed(){
   deathSpots.push(spot);
 }
 
+// shows text of where i clicked
 function showShot(){
   if (screen === "play"){
     for (let shot of deathSpots){
@@ -149,12 +161,15 @@ function showShot(){
   }
 }
 
+// ghost stuff pulled from set up to spawn ghosts and time their appearances
 function showGhosts(){
   imageMode(CENTER);
   spawnGhost();
   window.setInterval(spawnGhost, 500);
 }
 
+
+// removes the shot marking after a cerain wait time 
 function removeShots(){
   for (let shot of deathSpots){
     if (millis() > lastSwitchedTime + waitTime){
