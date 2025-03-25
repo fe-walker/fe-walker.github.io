2ee43d76cc27e9c67fa05f16cdb5ae631a7632ec
fe-walker <fewalker.ca@gmail.com>
2025-03-25 14:15:06 -0600

    finished demo

--- 11-game-of-life/sketch.js
+++ 11-game-of-life/sketch.js
@@ -1,9 +1,16 @@
 // Game of life demo
 
-const CELL_SIZE = 50;
+const CELL_SIZE = 20;
+const RENDER_ON_FRAME = 5;
 let grid;
 let rows;
 let cols;
+let autoPlayIsOn = false;
+// let gosper
+
+// function preload(){
+//   gosper = loadJSON(gosper-WebGLUniformLocation.json)
+// }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -14,8 +21,57 @@ function setup() {
 
 function draw() {
   background(220);
+  if (autoPlayIsOn && frameCount % RENDER_ON_FRAME === 0) {
+    grid = updateGrid();
+  }
   displayGrid();
 }
+
+function updateGrid() {
+  //make a new array to hold the new turn values
+  let nextTurn = generateEmptyGrid(cols, rows);
+
+  //look at every cell
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
+      let neighbours = 0;
+
+      //look at every neighbour
+      for (let i = -1; i <= 1; i++) {
+        for (let j = -1; j <= 1; j++) {
+          //don't fall off the edge of the grid
+          if (x+j >= 0 && x+j < cols && y+i >= 0 && y+i < rows) {
+            neighbours += grid[y+i][x+j];
+          }
+        }
+      }
+
+      //don't count self as neighbour
+      neighbours -= grid[y][x];
+
+      //apply the rules
+      if (grid[y][x] === 1) {  //currently alive
+        if (neighbours === 2 || neighbours === 3) {
+          nextTurn[y][x] = 1;
+        }
+        else {
+          nextTurn[y][x] = 0;
+        }
+      }
+
+      if (grid[y][x] === 0) {  //currently dead
+        if (neighbours === 3) {
+          nextTurn[y][x] = 1;
+        }
+        else {
+          nextTurn[y][x] = 0;
+        }
+      }
+    }
+  }
+  return nextTurn;
+}
+
 
 function displayGrid() {
   for (let y = 0; y < rows; y++) {
@@ -66,25 +122,26 @@ function keyPressed(){
   else if (key === "e"){
     grid = generateEmptyGrid(cols, rows);
   }
+  else if (key === "a") {
+    autoPlayIsOn = !autoPlayIsOn;
+  }
+  // else if (key === "g"){
+  //   grid = gosper 
+  // }
 }
 
-function mousePressed(){
+function mousePressed() {
   let x = Math.floor(mouseX/CELL_SIZE);
   let y = Math.floor(mouseY/CELL_SIZE);
 
   toggleCell(x, y);
-
-  toggleCell(x + 1, y);
-  toggleCell(x - 1, y);
-  toggleCell(x, y + 1);
-  toggleCell(x, y - 1);
 }
 
-function toggleCell(x, y){
-  if (grid[y][x] === 0){
+function toggleCell(x, y) {
+  if (grid[y][x] === 0) {
     grid[y][x] = 1;
   }
-  else if (grid[y][x]){
+  else if (grid[y][x] === 1) {
     grid[y][x] = 0;
   }
 }
