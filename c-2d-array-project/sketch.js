15dde5c1ce88dc965bbb8b25018f74937e5ac3a5
fe-walker <fewalker.ca@gmail.com>
2025-03-28 14:16:31 -0600

    added colour array, centered grid, made grid square

--- c-2d-array-project/sketch.js
+++ c-2d-array-project/sketch.js
@@ -6,21 +6,27 @@
 // - describe what you did to take this project "above and beyond"
 
 
-let cellSize; 
-const SQAURE_DIMENSIONS = 12;
+let cellSize = 60; 
+// increase cellSize per level
 let grid;
+let mainColour = [];
+
+// let secColour = {
+//   secCol1: "#2a3bdb",
+//   secCol2: "#c21324",
+//   secCol3: "#09630c",
+//   secCol4: "#f7f48f",
+// };
+// want to pull two colours, one from the main list and another thats either lighter or darker from the second list,
+//  randomly pick where to put the second colour and fill the rest of the grid with the first colour
+// let randomColour = random(220);
+let rows = 5;
+let cols = 5;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(cellSize * cols, cellSize * rows);
 
-  if (height > width){
-    cellSize = width/SQAURE_DIMENSIONS;
-  }
-  else{
-    cellSize = height/SQAURE_DIMENSIONS;
-  }
-
-  grid = generateRandomGrid(SQAURE_DIMENSIONS, SQAURE_DIMENSIONS);
+  grid = generateGrid(cols, rows);
 }
 
 function draw() {
@@ -28,27 +34,23 @@ function draw() {
 
   displayGrid();
 }
-function keyPressed(){
-  if (key === "r"){
-    grid = generateRandomGrid(SQAURE_DIMENSIONS, SQAURE_DIMENSIONS);
-  }
-  else if (key === "e"){
-    grid = generateGrid(SQAURE_DIMENSIONS, SQAURE_DIMENSIONS);
-  }
-}
+
+
 function displayGrid(){
-  for (let y = 0; y < SQAURE_DIMENSIONS; y++){
-    for (let x = 0; x < SQAURE_DIMENSIONS; x++){
-      if (grid[y][x] === 1){
-        fill("black");
-      }
-      else if (grid[y][x] === 0){
-        fill("white");
+  for (let y = 0; y < rows; y++){
+    for (let x = 0; x < cols; x++){
+      for (let colour of mainColour){
+        fillTheColour();
+        // figure out why this doesnt work
+        // need it to iterate through main colour array
+        fill(colour.col1);
+
       }
       rect(x*cellSize, y*cellSize, cellSize, cellSize);
     }
   }
 }
+
 
 function generateGrid(cols, rows){
   let newGrid = [];
@@ -61,35 +63,20 @@ function generateGrid(cols, rows){
   return newGrid;
 }
 
-function generateRandomGrid(cols, rows){
-  let newGrid = [];
-  for (let y = 0; y < rows; y ++){
-    newGrid.push([]);
-    for (let x = 0; x < cols; x ++){
-      if (random(100) > 50){
-        newGrid[y].push(0);
-      }
-      else{
-        newGrid[y].push(1);
-      }
-    }
-  }
-  return newGrid;
-}
+
 
 function mousePressed(){
   let x = Math.floor(mouseX/cellSize);
   let y = Math.floor(mouseY/cellSize);
 
-  toggleCell(x, y);
 }
 
-function toggleCell(x, y){
-  if (grid[y][x] === 0){
-    grid[y][x] = 1;
-  }
-  else if (grid[y][x]){
-    grid[y][x] = 0;
-  }
+function fillTheColour(){
+  let firstColour = {
+    col1: "#3446eb",
+    col2: "#c40c1e",
+    col3: "#09660c",
+    col4: "#f7f486",
+  };
+  mainColour.push(firstColour);
 }
-
