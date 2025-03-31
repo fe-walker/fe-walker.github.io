5bd4eb237407f35dc03f498c5999b4f6aafa9208
fe-walker <fewalker.ca@gmail.com>
2025-03-31 14:16:36 -0600

    fixed fill function

--- c-2d-array-project/sketch.js
+++ c-2d-array-project/sketch.js
@@ -7,21 +7,16 @@
 
 
 let cellSize = 60; 
-// increase cellSize per level
+// increase cellSize per level (1 level per colour)
 let grid;
-let mainColour = [];
-
-// let secColour = {
-//   secCol1: "#2a3bdb",
-//   secCol2: "#c21324",
-//   secCol3: "#09630c",
-//   secCol4: "#f7f48f",
-// };
-// want to pull two colours, one from the main list and another thats either lighter or darker from the second list,
-//  randomly pick where to put the second colour and fill the rest of the grid with the first colour
-// let randomColour = random(220);
 let rows = 5;
 let cols = 5;
+let mainColour = ["#3446eb", "#c40c1e", "#09660c","#f7f486"];
+let secondColour = ["#2a3bdb","#c21324","#09630c","#f7f48f",];
+let clickCount = 0;
+// want to pull two colours, one from the main list and another thats either lighter or darker from the second list,
+//  randomly pick where to put the second colour and fill the rest of the grid with the first colour
+
 
 function setup() {
   createCanvas(cellSize * cols, cellSize * rows);
@@ -39,13 +34,11 @@ function draw() {
 function displayGrid(){
   for (let y = 0; y < rows; y++){
     for (let x = 0; x < cols; x++){
-      for (let colour of mainColour){
-        fillTheColour();
-        // figure out why this doesnt work
-        // need it to iterate through main colour array
-        fill(colour.col1);
-
-      }
+      // for (let i = 0; i < mainColour.length; i++){
+      //   // need it to iterate through main colour array
+      //   fill(mainColour[i]);
+      //   console.log(mainColour[i]);
+      // }
       rect(x*cellSize, y*cellSize, cellSize, cellSize);
     }
   }
@@ -63,20 +56,46 @@ function generateGrid(cols, rows){
   return newGrid;
 }
 
+// function generateRandomGrid(cols, rows) {
+//   let newGrid = [];
+//   for (let y = 0; y < rows; y++) {
+//     newGrid.push([]);
+//     for (let x = 0; x < cols; x++) {
+//       //toss a 0 or 1 in randomly
+//       if (random(100) < 50) {
+//         newGrid[y].push(OPEN_TILE);
+//       }
+//       else {
+//         newGrid[y].push(IMPASSIBLE);
+//       }
+//     }
+//   }
+//   return newGrid;
+// }
+
+// would use a function like that with open/closed space style, but that makes more than one open/closed space, when i need only one closed space
+// while the rest remain open spaces. 
+
 
 
 function mousePressed(){
   let x = Math.floor(mouseX/cellSize);
   let y = Math.floor(mouseY/cellSize);
 
+  // if the position of the mouse is clicked in the same spot as the colour thats different, change values, otherwise dont 
+  cellSize = cellSize - 5;
+  cols = cols + 2;
+  rows = rows + 2;
+
+
+  changeFillColour();
+  clickCount++;
 }
 
-function fillTheColour(){
-  let firstColour = {
-    col1: "#3446eb",
-    col2: "#c40c1e",
-    col3: "#09660c",
-    col4: "#f7f486",
-  };
-  mainColour.push(firstColour);
+function changeFillColour(){
+  // for (let i = 0; i < mainColour.length; i++){
+  // need it to iterate through main colour array
+  fill(mainColour[clickCount]);
+  console.log(mainColour[clickCount]);
+  // }
 }
