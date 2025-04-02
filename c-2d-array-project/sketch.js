72f389dcab0c1115c96595228a6ae91b93a871fa
fe-walker <fewalker.ca@gmail.com>
2025-04-02 14:15:30 -0600

    changed some global variables, addded some functions, changed state variable function

--- c-2d-array-project/sketch.js
+++ c-2d-array-project/sketch.js
@@ -20,14 +20,15 @@ let cellSize = 60;
 let grid;
 let rows = 10;
 let cols = 10;
-let mainColour = ["white","#3446eb", "#c40c1e", "#09660c","#f7f486", "#e36dc7", "#ffc8ab", "#abeaff", "#093d4f", "#8d1cba"];
+let mainColour = ["#3446eb", "#c40c1e", "#09660c","#f7f486", "#e36dc7", "#ffc8ab", "#abeaff", "#093d4f", "#8d1cba"];
 // still need to decide amount of levels, can go up to 25 but it gets laggy
-let secondColour = ["white","#2a3bdb","#c21324","#09630c","#f0ed81","#e065c4", "#ffcdb3", "#a3ddf0", "#053040", "#9623c4"];
-let clickCount = 0;
-let attempts = 0;
-let screen = "start";
-const OPEN_TILE = 0;
-const IMPASSIBLE = 1;
+let secondColour = ["#2a3bdb","#c21324","#09630c","#f0ed81","#e065c4", "#ffcdb3", "#a3ddf0", "#053040", "#9623c4"];
+let clickCount;
+let attempts;
+let screen = "intro";
+const MAIN = 0;
+const ODD = 1;
+let spot = 1;
 // want to pull two colours, one from the main list and another thats either lighter or darker from the second list,
 //  randomly pick where to put the second colour and fill the rest of the grid with the first colour
 
@@ -41,11 +42,22 @@ function setup() {
 function draw() {
   background(220);
 
-  displayGrid();
+  changeScreen();
 }
 
 function changeScreen(){
-  if (screen === "won"){
+  if(screen === "intro"){
+    if (keyCode === 13){
+      screen = "start";
+    }
+  }
+  else if (screen === "start"){
+
+    clickCount = 0;
+    attempts = 0;
+    displayGrid();
+  }
+  else if (screen === "won"){
 
   }
   else if (screen === "lost"){
@@ -56,10 +68,10 @@ function changeScreen(){
 function displayGrid(){
   for (let y = 0; y < rows; y++){
     for (let x = 0; x < cols; x++){
-      if (random(100) < 50 ){
+      if (grid[y][x] === MAIN){
         fill(mainColour[clickCount]);
       }
-      else{
+      else if (grid[y][x] === ODD){
         fill(secondColour[clickCount]);
       }
       rect(x*cellSize, y*cellSize, cellSize, cellSize);
@@ -73,7 +85,13 @@ function generateGrid(cols, rows){
   for (let y = 0; y < rows; y ++){
     newGrid.push([]);
     for (let x = 0; x < cols; x ++){
-      newGrid.push([0]);
+      // pick one spot for the odd one out 
+      if (spot === 1){
+        newGrid.push([ODD]);
+      }
+      else{
+        newGrid.push([MAIN]);
+      }
     }
   }
   return newGrid;
@@ -83,11 +101,22 @@ function generateGrid(cols, rows){
 
 
 function mousePressed(){
+
+  // pretty sure this math doesnt work for my code, need to change it 
+
+  if (screen === "start"){
+
+    changeLevelIfNeeded();
+  }
+
+}
+
+function changeLevelIfNeeded(){
   let x = Math.floor(mouseX/cellSize);
   let y = Math.floor(mouseY/cellSize);
   for (let i = 0; i < rows; i++){
     for (let j = 0; j < cols; j++){
-      if (x === && y ===){ 
+      if (x === ODD && y === ODD){ 
         // dont know what this argument will be yet; depends on my second colour position
         cellSize = cellSize - 2;
         cols = cols + 2;
@@ -108,7 +137,6 @@ function mousePressed(){
       }
     }
   }
-
 }
 
 function changeFillColour(){
