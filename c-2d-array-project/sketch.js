5d56cce550303d5a12696d0c0680e4652d5056ba
fe-walker <fewalker.ca@gmail.com>
2025-04-04 14:17:36 -0600

    changed attempt variable, added and took out a couple nested loops, seperated 1 function into 2 functions

--- c-2d-array-project/sketch.js
+++ c-2d-array-project/sketch.js
@@ -17,11 +17,10 @@ let cols = 10;
 let mainColour = ["#3446eb", "#c40c1e", "#09660c","#f7f486", "#e36dc7", "#ffc8ab", "#abeaff", "#093d4f", "#8d1cba", "#3446eb", "#c40c1e", "#09660c","#f7f486", "#e36dc7", "#ffc8ab", "#abeaff", "#093d4f", "#8d1cba"];
 let secondColour = ["#2a3bdb","#c21324","#09630c","#f0ed81","#e065c4", "#ffcdb3", "#a3ddf0", "#053040", "#9623c4", "#2a3bdb","#c21324","#09630c","#f0ed81","#e065c4", "#ffcdb3", "#a3ddf0", "#053040", "#9623c4"];
 let clickCount = 0;
-let attempts;
+let attempts = 0;
 let screen = "intro";
 const MAIN = 0;
 const ODD = 1;
-let spot;
 
 
 function setup() {
@@ -45,11 +44,12 @@ function changeScreen(){
   }
   else if (screen === "start"){
     console.log(screen);
-    // clickCount = 0;
-    // attempts = 0;
     displayGrid();
     if (attempts === 5){
       screen = "lost";
+    }
+    if (clickCount === 18){
+      screen = "won";
     }
   }
   else if (screen === "won"){
@@ -64,6 +64,7 @@ function changeScreen(){
 function displayGrid(){
   for (let y = 0; y < rows; y++){
     for (let x = 0; x < cols; x++){
+      console.log(x, y, mainColour, clickCount);
       if (grid[y][x] === MAIN){
         fill(mainColour[clickCount]);
       }
@@ -81,14 +82,11 @@ function generateGrid(cols, rows){
   for (let y = 0; y < rows; y ++){
     newGrid.push([]);
     for (let x = 0; x < cols; x ++){
-      // pick one spot for the odd one out 
       if (random(10) > 5){
-        newGrid.push([ODD]);
-        spot = ODD;
+        newGrid[y].push(ODD);
       }
       else{
-        newGrid.push([MAIN]);
-        spot = MAIN;
+        newGrid[y].push(MAIN);
       }
     }
   }
@@ -99,46 +97,47 @@ function generateGrid(cols, rows){
 
 
 function mousePressed(){
-
-
   if (screen === "start"){
-
-    changeLevelIfNeeded();
+    let x = Math.floor(mouseX/cellSize);
+    let y = Math.floor(mouseY/cellSize);
+    console.log("grid spot is " + grid[y][x]);
+    console.log("x spot is " + x);
+    console.log("y spot is " + y);
+    if (grid[y][x] === ODD){
+      changeLevelIfNeeded();
+    }
+    else if (grid[y][x] === MAIN){
+      stayOnLevel();
+    }
   }
-
 }
 
 function changeLevelIfNeeded(){
-  let x = Math.floor(mouseX/cellSize);
-  let y = Math.floor(mouseY/cellSize);
-  if (spot === ODD){ 
-  // need to change because x & y r not 1 or 0, they're the co-ordinates for the spot on the grid
-    cellSize = cellSize - 2;
-    cols = cols + 2;
-    rows = rows + 2;
-      
-    changeFillColour();
-    clickCount++;
-    // console.log(clickCount);
-    console.log(x);
-    console.log(y);
-  }
-  else{
-    // clickCount = clickCount - 1;
-    attempts++;
-  }
+  cellSize = cellSize - 2;
+  cols = cols + 2;
+  rows = rows + 2;
+    
+  // changeFillColour();
+  clickCount++;
+  // console.log(clickCount);
+  // console.log("x spot is " + x);
+  // console.log("y spot is " + y);
+
+
+}
+
+function stayOnLevel(){
+  // clickCount = clickCount -1;
+  attempts++;
+  console.log("attempts is " + attempts);
 }
 
 function changeFillColour(){
-  for (let y = 0; y < rows; y++){
-    for (let x = 0; x < cols; x++){
-      if (grid[y][x] === MAIN){
-        fill(mainColour[clickCount]);
-      }
-      else if (grid[y][x] === ODD){
-        fill(secondColour[clickCount]);
-      }
-    }
+  if (grid[y][x] === MAIN){
+    fill(mainColour[clickCount]);
+  }
+  else if (grid[y][x] === ODD){
+    fill(secondColour[clickCount]);
   }
 
   console.log(mainColour[clickCount]);
