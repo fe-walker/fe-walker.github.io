2ff18b5fe7163adcd5f74909e8bdc71fc3ef937a
fe-walker <fewalker.ca@gmail.com>
2025-04-07 14:15:21 -0600

    added some comments, rearranged some things, fixed the error displaying my grid

--- c-2d-array-project/sketch.js
+++ c-2d-array-project/sketch.js
@@ -1,26 +1,32 @@
 // Colour Blind Game
 // Faith Walker
-// 4/9/2025
+// 4/10/2025
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
 // to-do
-// finish state variables
+// finish state variables (need to add text and stuff still)
 // style.css stuff 
 
+// set constants
+const MAIN = 0;
+const ODD = 1;
 
+// set variables
 let cellSize = 60; 
 let grid;
 let rows = 10;
 let cols = 10;
-let mainColour = ["#3446eb", "#c40c1e", "#09660c","#f7f486", "#e36dc7", "#ffc8ab", "#abeaff", "#093d4f", "#8d1cba", "#3446eb", "#c40c1e", "#09660c","#f7f486", "#e36dc7", "#ffc8ab", "#abeaff", "#093d4f", "#8d1cba"];
-let secondColour = ["#2a3bdb","#c21324","#09630c","#f0ed81","#e065c4", "#ffcdb3", "#a3ddf0", "#053040", "#9623c4", "#2a3bdb","#c21324","#09630c","#f0ed81","#e065c4", "#ffcdb3", "#a3ddf0", "#053040", "#9623c4"];
 let clickCount = 0;
 let attempts = 0;
 let screen = "intro";
-const MAIN = 0;
-const ODD = 1;
+
+//colours to iterate through 
+let mainColour = ["#3446eb", "#c40c1e", "#09660c","#f7f486", "#e36dc7", "#ffc8ab", "#abeaff", "#093d4f", "#8d1cba", 
+  "#3446eb", "#c40c1e", "#09660c","#f7f486", "#e36dc7", "#ffc8ab", "#abeaff", "#093d4f", "#8d1cba"];
+let secondColour = ["#2a3bdb","#c21324","#09630c","#f0ed81","#e065c4", "#ffcdb3", "#a3ddf0", "#053040", "#9623c4", 
+  "#2a3bdb","#c21324","#09630c","#f0ed81","#e065c4", "#ffcdb3", "#a3ddf0", "#053040", "#9623c4"];
 
 
 function setup() {
@@ -35,6 +41,7 @@ function draw() {
   changeScreen();
 }
 
+// state variables that change if you win/lose
 function changeScreen(){
   if(screen === "intro"){
     if (keyCode === 13){
@@ -54,6 +61,7 @@ function changeScreen(){
   }
   else if (screen === "won"){
     console.log(screen);
+    // happens when you've succesfully gone through every level with less than 5 missed attempts
   }
   else if (screen === "lost"){
     console.log(screen);
@@ -64,7 +72,7 @@ function changeScreen(){
 function displayGrid(){
   for (let y = 0; y < rows; y++){
     for (let x = 0; x < cols; x++){
-      console.log(x, y, mainColour, clickCount);
+      // console.log(x, y);
       if (grid[y][x] === MAIN){
         fill(mainColour[clickCount]);
       }
@@ -75,7 +83,6 @@ function displayGrid(){
     }
   }
 }
-
 
 function generateGrid(cols, rows){
   let newGrid = [];
@@ -93,14 +100,12 @@ function generateGrid(cols, rows){
   return newGrid;
 }
 
-
-
-
 function mousePressed(){
+  // changes the level if you click the correct colour, stays on the current level if you pick the wrong colour
   if (screen === "start"){
     let x = Math.floor(mouseX/cellSize);
     let y = Math.floor(mouseY/cellSize);
-    console.log("grid spot is " + grid[y][x]);
+    console.log("main or odd; " + grid[y][x]);
     console.log("x spot is " + x);
     console.log("y spot is " + y);
     if (grid[y][x] === ODD){
@@ -116,7 +121,9 @@ function changeLevelIfNeeded(){
   cellSize = cellSize - 2;
   cols = cols + 2;
   rows = rows + 2;
-    
+  
+  grid = generateGrid(cols, rows);
+
   // changeFillColour();
   clickCount++;
   // console.log(clickCount);
@@ -133,6 +140,7 @@ function stayOnLevel(){
 }
 
 function changeFillColour(){
+  // changes the fill colour of the grid
   if (grid[y][x] === MAIN){
     fill(mainColour[clickCount]);
   }
