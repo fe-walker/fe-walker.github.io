95627fc60c97629f64fd92a30601525ca3e73dd0
fe-walker <fewalker.ca@gmail.com>
2025-05-12 14:08:47 -0600

    did demo

--- 20-2d-collide/sketch.js
+++ 20-2d-collide/sketch.js
@@ -1,11 +1,26 @@
 // 2d collide
 
-
+let hit = false;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(500,500);
 }
 
+
+
 function draw() {
-  background(220);
+  background(255);
+  ellipse(200, 200, 50, 150);
+  point(mouseX, mouseY);
+
+  hit = collidePointEllipse(mouseX, mouseY, 200, 200, 50, 150);
+
+  // Use vectors as input:
+  // const mouse         = createVector(mouseX, mouseY);
+  // const ellipse_start = createVector(200, 200);
+  // const ellipse_size  = createVector(50, 150);
+  // hit = collidePointEllipseVector(mouse, ellipse_start, ellipse_size);
+
+  stroke(hit ? color('red') : 0);
+  print('colliding?', hit);
 }
